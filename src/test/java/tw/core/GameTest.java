0c02af684065c84f4c7a26be505e55559cda03da
zdazx <zhangduo0227@163.com>
2019-04-26 16:28:46 +0800

    RandomIntGenerator Test

--- src/test/java/tw/core/GameTest.java
+++ src/test/java/tw/core/GameTest.java
@@ -1,10 +1,19 @@
 package tw.core;
 
+import com.google.inject.Inject;
+import org.junit.Test;
+import tw.core.exception.OutOfRangeAnswerException;
+import tw.core.generator.AnswerGenerator;
+
 /**
  * 在GameTest文件中完成Game中对应的单元测试
  */
 
 
 public class GameTest {
+    @Test
+    public void should_return_guessed_result() throws OutOfRangeAnswerException {
+//        Game game = new Game(AnswerGenerator answerGenerator);
+    }
 
 }
--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -1,9 +1,21 @@
 package tw.core;
 
 
+import org.junit.Test;
+import tw.core.generator.RandomIntGenerator;
+
+import static org.mockito.Mockito.*;
+
 /**
  * 在RandomIntGeneratorTest文件中完成RandomIntGenerator中对应的单元测试
  */
 public class RandomIntGeneratorTest {
+
+    @Test
+    public void should_generate_nums(){
+        RandomIntGenerator randomIntGenerator = mock(RandomIntGenerator.class);
+        randomIntGenerator.generateNums(9,4);
+        verify(randomIntGenerator,times(1)).generateNums(9,4);
+    }
 
 }
