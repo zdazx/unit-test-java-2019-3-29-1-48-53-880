f410f197fccb6aed207c5f72a9563f09a7c34b86
zdazx <zhangduo0227@163.com>
2019-04-26 17:32:43 +0800

    GameController Test

--- src/test/java/tw/controllers/GameControllerTest.java
+++ src/test/java/tw/controllers/GameControllerTest.java
@@ -1,8 +1,23 @@
 package tw.controllers;
 
+import org.junit.Test;
+import tw.commands.GuessInputCommand;
+import tw.commands.InputCommand;
+
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
+
 /**
  * 在GameControllerTest文件中完成GameController中对应的单元测试
  */
 public class GameControllerTest {
+    @Test
+    public void should_play() throws IOException {
+        GameController gameController = mock(GameController.class);
+        InputCommand command = new GuessInputCommand();
+        gameController.play(command);
+        verify(gameController,times(1)).play(command);
+    }
 
 }
